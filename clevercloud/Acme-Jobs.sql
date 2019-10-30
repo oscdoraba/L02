b86275887153d68169d2345cccd41e13b5c4f259
Oscar <oscdoraba@alum.us.es>
2019-10-30 13:28:30 +0100

    Subiendo el proyecto de la primera practica.

---
+++ clevercloud/Acme-Jobs.sql
@@ -0,0 +1,405 @@
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: acme-jobs
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `administrator`
+--
+
+DROP TABLE IF EXISTS `administrator`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `administrator` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `user_account_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_2a5vcjo3stlfcwadosjfq49l1` (`user_account_id`),
+  CONSTRAINT `FK_2a5vcjo3stlfcwadosjfq49l1` FOREIGN KEY (`user_account_id`) REFERENCES `user_account` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `administrator`
+--
+
+LOCK TABLES `administrator` WRITE;
+/*!40000 ALTER TABLE `administrator` DISABLE KEYS */;
+INSERT INTO `administrator` VALUES (4,0,3);
+/*!40000 ALTER TABLE `administrator` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `announcement`
+--
+
+DROP TABLE IF EXISTS `announcement`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `announcement` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `moment` datetime(6) DEFAULT NULL,
+  `more_info` varchar(255) DEFAULT NULL,
+  `text` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `announcement`
+--
+
+LOCK TABLES `announcement` WRITE;
+/*!40000 ALTER TABLE `announcement` DISABLE KEYS */;
+/*!40000 ALTER TABLE `announcement` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `anonymous`
+--
+
+DROP TABLE IF EXISTS `anonymous`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `anonymous` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `user_account_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_6lnbc6fo3om54vugoh8icg78m` (`user_account_id`),
+  CONSTRAINT `FK_6lnbc6fo3om54vugoh8icg78m` FOREIGN KEY (`user_account_id`) REFERENCES `user_account` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `anonymous`
+--
+
+LOCK TABLES `anonymous` WRITE;
+/*!40000 ALTER TABLE `anonymous` DISABLE KEYS */;
+INSERT INTO `anonymous` VALUES (2,0,1);
+/*!40000 ALTER TABLE `anonymous` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `authenticated`
+--
+
+DROP TABLE IF EXISTS `authenticated`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `authenticated` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `user_account_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_h52w0f3wjoi68b63wv9vwon57` (`user_account_id`),
+  CONSTRAINT `FK_h52w0f3wjoi68b63wv9vwon57` FOREIGN KEY (`user_account_id`) REFERENCES `user_account` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `authenticated`
+--
+
+LOCK TABLES `authenticated` WRITE;
+/*!40000 ALTER TABLE `authenticated` DISABLE KEYS */;
+INSERT INTO `authenticated` VALUES (5,0,3);
+/*!40000 ALTER TABLE `authenticated` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `calle_perez_bulletin`
+--
+
+DROP TABLE IF EXISTS `calle_perez_bulletin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `calle_perez_bulletin` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `author` varchar(255) DEFAULT NULL,
+  `body` varchar(255) DEFAULT NULL,
+  `moment` datetime(6) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `calle_perez_bulletin`
+--
+
+LOCK TABLES `calle_perez_bulletin` WRITE;
+/*!40000 ALTER TABLE `calle_perez_bulletin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `calle_perez_bulletin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `campos_iglesia_bulletin`
+--
+
+DROP TABLE IF EXISTS `campos_iglesia_bulletin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `campos_iglesia_bulletin` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `dorsal` int(11) DEFAULT NULL,
+  `equipo` varchar(255) DEFAULT NULL,
+  `nacimiento` datetime(6) DEFAULT NULL,
+  `patrocinador` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `campos_iglesia_bulletin`
+--
+
+LOCK TABLES `campos_iglesia_bulletin` WRITE;
+/*!40000 ALTER TABLE `campos_iglesia_bulletin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `campos_iglesia_bulletin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `consumer`
+--
+
+DROP TABLE IF EXISTS `consumer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `consumer` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `user_account_id` int(11) DEFAULT NULL,
+  `company` varchar(255) DEFAULT NULL,
+  `sector` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_6cyha9f1wpj0dpbxrrjddrqed` (`user_account_id`),
+  CONSTRAINT `FK_6cyha9f1wpj0dpbxrrjddrqed` FOREIGN KEY (`user_account_id`) REFERENCES `user_account` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `consumer`
+--
+
+LOCK TABLES `consumer` WRITE;
+/*!40000 ALTER TABLE `consumer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `consumer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dorado_abadias_bulletin`
+--
+
+DROP TABLE IF EXISTS `dorado_abadias_bulletin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dorado_abadias_bulletin` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `author` varchar(255) DEFAULT NULL,
+  `oid` int(11) DEFAULT NULL,
+  `text` varchar(255) DEFAULT NULL,
+  `uri` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dorado_abadias_bulletin`
+--
+
+LOCK TABLES `dorado_abadias_bulletin` WRITE;
+/*!40000 ALTER TABLE `dorado_abadias_bulletin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dorado_abadias_bulletin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ganan_garcia_bulletin`
+--
+
+DROP TABLE IF EXISTS `ganan_garcia_bulletin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ganan_garcia_bulletin` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `dorsal` int(11) DEFAULT NULL,
+  `fecha_inscripcion` datetime(6) DEFAULT NULL,
+  `goles` int(11) DEFAULT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `partidos_jugados` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ganan_garcia_bulletin`
+--
+
+LOCK TABLES `ganan_garcia_bulletin` WRITE;
+/*!40000 ALTER TABLE `ganan_garcia_bulletin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ganan_garcia_bulletin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hibernate_sequence`
+--
+
+DROP TABLE IF EXISTS `hibernate_sequence`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hibernate_sequence`
+--
+
+LOCK TABLES `hibernate_sequence` WRITE;
+/*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
+INSERT INTO `hibernate_sequence` VALUES (6);
+/*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `provider`
+--
+
+DROP TABLE IF EXISTS `provider`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `provider` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `user_account_id` int(11) DEFAULT NULL,
+  `company` varchar(255) DEFAULT NULL,
+  `sector` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_b1gwnjqm6ggy9yuiqm0o4rlmd` (`user_account_id`),
+  CONSTRAINT `FK_b1gwnjqm6ggy9yuiqm0o4rlmd` FOREIGN KEY (`user_account_id`) REFERENCES `user_account` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `provider`
+--
+
+LOCK TABLES `provider` WRITE;
+/*!40000 ALTER TABLE `provider` DISABLE KEYS */;
+/*!40000 ALTER TABLE `provider` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sanchez_munoz_bulletin`
+--
+
+DROP TABLE IF EXISTS `sanchez_munoz_bulletin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sanchez_munoz_bulletin` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `author` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `quality` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sanchez_munoz_bulletin`
+--
+
+LOCK TABLES `sanchez_munoz_bulletin` WRITE;
+/*!40000 ALTER TABLE `sanchez_munoz_bulletin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sanchez_munoz_bulletin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `shout`
+--
+
+DROP TABLE IF EXISTS `shout`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `shout` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `author` varchar(255) DEFAULT NULL,
+  `moment` datetime(6) DEFAULT NULL,
+  `text` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shout`
+--
+
+LOCK TABLES `shout` WRITE;
+/*!40000 ALTER TABLE `shout` DISABLE KEYS */;
+/*!40000 ALTER TABLE `shout` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_account`
+--
+
+DROP TABLE IF EXISTS `user_account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_account` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `enabled` bit(1) NOT NULL,
+  `identity_email` varchar(255) DEFAULT NULL,
+  `identity_name` varchar(255) DEFAULT NULL,
+  `identity_surname` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_castjbvpeeus0r8lbpehiu0e4` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_account`
+--
+
+LOCK TABLES `user_account` WRITE;
+/*!40000 ALTER TABLE `user_account` DISABLE KEYS */;
+INSERT INTO `user_account` VALUES (1,0,_binary '\0','john.doe@acme.com','John','Doe','$2a$05$6C1yau0qTyrMBFtVFtEdwub0dLVhZ2ZUcX9e2eOUJ0zkRSRracLyq','anonymous'),(3,0,_binary '','administrator@acme.com','Administrator','Acme.com','$2a$05$s6JbpBcKxqkGtsDezJzmOOGsz/P4dYej/e4CmN5n5XQMXPeOTAkJO','administrator');
+/*!40000 ALTER TABLE `user_account` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-10-30 13:17:29
