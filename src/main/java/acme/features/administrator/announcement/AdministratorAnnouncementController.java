0f8449cdd262809aa968f40ce878eb4dda2f0bd2
Oscar <oscdoraba@alum.us.es>
2019-11-04 15:28:45 +0100

    Task-004: Finished, investigadores creado y ciertos cambios en el proyecto que llevaban a error

---
+++ src/main/java/acme/features/administrator/announcement/AdministratorAnnouncementController.java
@@ -0,0 +1,47 @@
+/*
+ * AuthenticatedConsumerController.java
+ *
+ * Copyright (c) 2019 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.features.administrator.announcement;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import acme.entities.announcements.Announcement;
+import acme.framework.components.BasicCommand;
+import acme.framework.controllers.AbstractController;
+import acme.framework.entities.Administrator;
+
+@Controller
+@RequestMapping("/administrator/announcement/")
+public class AdministratorAnnouncementController extends AbstractController<Administrator, Announcement> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AdministratorAnnouncementListService	listService;
+
+	@Autowired
+	private AdministratorAnnouncementShowService	showService;
+
+
+	// Constructors -----------------------------------------------------------
+
+	@PostConstruct
+	private void initialise() {
+		super.addBasicCommand(BasicCommand.LIST, this.listService);
+		super.addBasicCommand(BasicCommand.SHOW, this.showService);
+	}
+
+}
